2a88992b60a2e5f61260334ce2f8fadb2f4f2f37
mohammed medhat <mohamed.medhat2121@gmail.com>
2024-03-04 00:36:41 +0200

    add pipes

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 async function bootstrap(): Promise<void> {
-  const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
-  await app.listen(process.env.PORT || 3000);
+	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(
+		new ValidationPipe({
+			transform: true,
+			whitelist: true,
+			forbidNonWhitelisted: true,
+		}),
+	);
+	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
